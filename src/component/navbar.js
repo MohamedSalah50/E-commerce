b7dfa226d12f2ed59293abfef91498a97dc81019
abdelaziz <abdelazizelhadry1@gmail.com>
2023-12-14 23:14:03 +0200

    update

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,40 +1,29 @@
-import {Link} from "react-router-dom"
-function Navbar() {
+import { Link } from "react-router-dom";
+import { Nav, Container, Navbar, NavbarBrand, NavItem } from "react-bootstrap";
+
+function NavBar() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container">
-          <Link className="navbar-brand" to={"/"}>
-            logo
+    <Navbar>
+      <Container>
+        <NavbarBrand>
+          <Link to={"/"} className="nav-link">
+            LOGO
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to={"/"}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/about"}>
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+        </NavbarBrand>
+        <Nav className="ms-auto ">
+          <NavItem>
+            <Link to={"/home"} className="mx-3 nav-link">
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to={"/about"} className="nav-link">
+              About
+            </Link>
+          </NavItem>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
-export default Navbar;
+export default NavBar;
